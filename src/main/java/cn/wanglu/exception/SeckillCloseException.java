43853e30b20bff67703bfd37cdd7f6d4d5986edc
wanglu <376638623@qq.com>
2020-10-04 14:11:39 +0900

    secondSkill

---
+++ src/main/java/cn/wanglu/exception/SeckillCloseException.java
@@ -0,0 +1,15 @@
+package cn.wanglu.exception;
+
+/**
+ * 秒杀关闭异常，当秒杀结束时用户还要进行秒杀就会出现这个异常
+ * Created by wanglu on 16/11/27.
+ */
+public class SeckillCloseException extends SeckillException{
+    public SeckillCloseException(String message) {
+        super(message);
+    }
+
+    public SeckillCloseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
